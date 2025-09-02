fc9866457f825827621cbbcca94b5169dbaab5ed
Omar-Alme <omar.almehdar17@gmail.com>
2025-09-02 10:42:43 +0200

    fix: update main background color to blue-900

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default async function Home() {
   const todos = await db.todo.findMany();
 
   return (
-    <main className="p-4  bg-amber-600">
+    <main className="p-4 bg-blue-900">
       <TodoList defaultTodos={todos} />
       <h1 className="text-3xl bg-amber-600">Test CI</h1>
     </main>
